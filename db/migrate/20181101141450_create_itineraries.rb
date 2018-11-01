c226ff1e30a6c10e08bf473891f8e98d6f6a2cdf
Aaron J. Smith <onsmith@cs.unc.edu>
2018-11-01 15:24:02 -0400

    Removed "is_purchased" flag from itineraries table

--- db/migrate/20181101141450_create_itineraries.rb
+++ db/migrate/20181101141450_create_itineraries.rb
@@ -7,9 +7,6 @@ class CreateItineraries < ActiveRecord::Migration[5.1]
       ## Purchaser information
       t.string :email
 
-      ## Purchase status
-      t.boolean :is_purchased, null: false, default: false
-
       ## Might belong to a student
       t.belongs_to :user, index: true
 
